b7c6dadae874bc4e04f0340f0bb0370b290b59c2
Handyg11 <handygoandy@gmail.com>
2025-03-10 11:15:41 +0700

    update
    update from 8 march

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -7,14 +7,21 @@ public class PlayerMovement : MonoBehaviour
     public float speed, runMultiplier;
     private Rigidbody2D rb2d;
     private Vector2 input;
+    private Vector2 mouseDirection; // Stores direction toward the mouse
 
-    // Start is called before the first frame update
+    // Melee Attack
+    public Transform attackPoint;
+    public float attackRange = 0.5f;
+    public int damage = 1;
+    public LayerMask enemyLayers;
+    public float attackCooldown = 0.5f;
+    private float nextAttackTime = 0f;
+
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         float currSpeed = speed;
@@ -22,18 +29,78 @@ public class PlayerMovement : MonoBehaviour
         {
             currSpeed *= runMultiplier;
         }
-        else
-        {
 
-        }
+        // Player movement
         input.x = Input.GetAxisRaw("Horizontal") * currSpeed;
         input.y = Input.GetAxisRaw("Vertical") * currSpeed;
+
+        // Rotate player toward the mouse
+        RotateTowardsMouse();
+
+        // Attack
+        if (Time.time >= nextAttackTime)
+        {
+            if (Input.GetKeyDown(KeyCode.Space)) // Change to your attack button
+            {
+                Attack();
+                nextAttackTime = Time.time + attackCooldown;
+            }
+        }
+    }
+
+    void RotateTowardsMouse()
+    {
+        // Get mouse position in world space
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePosition.z = 0; // Ensure it's in 2D space
+
+        // Get direction from player to mouse
+        mouseDirection = (mousePosition - transform.position).normalized;
+
+        // Calculate angle and apply rotation
+        float angle = Mathf.Atan2(mouseDirection.y, mouseDirection.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, angle);
+    }
+
+    void Attack()
+    {
+        // Detect enemies in attack range
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
+
+        foreach (Collider2D enemy in hitEnemies)
+        {
+            Vector2 directionToEnemy = (enemy.transform.position - transform.position).normalized;
+
+            // Player's current forward direction is now based on mouse rotation
+            Vector2 playerForward = mouseDirection;
+
+            float angle = Vector2.SignedAngle(playerForward, directionToEnemy);
+
+            if (angle >= -45 && angle <= 45)
+            {
+                Debug.Log("Hit FRONT of " + enemy.name);
+                //enemy.GetComponent<EnemyHealth>()?.TakeDamage(damage);
+            }
+            else if (angle > 45 && angle < 135)
+            {
+                Debug.Log("Hit RIGHT SIDE of " + enemy.name);
+                //enemy.GetComponent<EnemyHealth>()?.TakeDamage(damage);
+            }
+            else if (angle < -45 && angle > -135)
+            {
+                Debug.Log("Hit LEFT SIDE of " + enemy.name);
+                //enemy.GetComponent<EnemyHealth>()?.TakeDamage(damage);
+            }
+            else
+            {
+                Debug.Log("Hit BACK of " + enemy.name);
+                //enemy.GetComponent<EnemyHealth>()?.TakeDamage(damage);
+            }
+        }
     }
 
     private void FixedUpdate()
     {
-        //rb2d.velocity = new Vector2(input.x, input.y);
         rb2d.velocity = input;
-
     }
 }
